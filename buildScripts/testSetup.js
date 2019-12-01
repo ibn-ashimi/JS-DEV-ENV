81f369f9d8505047c4dc5f9dd22d0e8b83df99e1
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2019-12-02 00:13:34 +0100

    Javascript Development Environment

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,5 @@
+//This file isn't transpiled, so must use commonJS and Es5
+//Register babel to transpile before our test run
+require('babel-register')();
+//Disable webpack feature that Mocha doesn't understand
+require.extensions['.css'] = function(){};
